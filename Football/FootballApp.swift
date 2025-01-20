319053c517121d138e52626fba91357e34f1244a
Derdrom-TJM <94881069+Derdrom-TJM@users.noreply.github.com>
2025-01-20 14:22:19 -0600

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Todd McFarlin on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
